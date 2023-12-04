7d4f46a3132c00ef37478ffcf6d44988bed7c5d5
Afridi Akbar Ifty <afridiakbarifty@gmail.com>
2023-12-04 22:33:22 +0600

    Cart reading Successful

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
-require('dotenv').config()
+require("dotenv").config();
 const port = process.env.PORT || 5000;
-
 
 // MiddleWare
 app.use(cors());
@@ -11,7 +10,7 @@ app.use(express.json());
 
 // MONGOBD STARTS HERE
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7grn8zj.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -20,7 +19,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -28,23 +27,42 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-const serviceCollection = client.db('elaraTravels').collection('services');
-const reviewsollection = client.db('elaraTravels').collection('reviews');
+    const serviceCollection = client.db("elaraTravels").collection("services");
+    const reviewsCollection = client.db("elaraTravels").collection("reviews");
+    const cartsCollection = client.db("elaraTravels").collection("carts");
 
-// getting the Service data
-app.get('/services',async(req,res)=>{
-    const result = await serviceCollection.find().toArray();
-    res.send(result);
-})
-// getting the Reviews data
-app.get('/reviews',async(req,res)=>{
-    const result = await reviewsollection.find().toArray();
-    res.send(result);
-})
+    // getting the Service data
+    app.get("/services", async (req, res) => {
+      const result = await serviceCollection.find().toArray();
+      res.send(result);
+    });
+    // getting the Reviews data
+    app.get("/reviews", async (req, res) => {
+      const result = await reviewsCollection.find().toArray();
+      res.send(result);
+    });
+
+    // Carts Collection
+    // creating here
+    app.post("/carts", async (req, res) => {
+      const cartItem = req.body;
+      const result = await cartsCollection.insertOne(cartItem);
+      res.send(result);
+    });
+
+    // Reading here
+    app.get("/carts", async (req, res) => {
+        const email = req.query.email;
+        const query= {email: email};
+      const result = await cartsCollection.find(query).toArray();
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -52,14 +70,11 @@ app.get('/reviews',async(req,res)=>{
 }
 run().catch(console.dir);
 
+// MOPNGODB ENDS HERE
 
-
-
-// MOPNGODB ENDS HERE 
-
-app.get('/',(req,res)=>{
-    res.send('ELARA TRAVEL IS TRAVELLING')
-})
-app.listen(port, ()=>{
-    console.log(`ELARA TRAVEL IS TRAVELLING ON PORT ${port}`);
-})
+app.get("/", (req, res) => {
+  res.send("ELARA TRAVEL IS TRAVELLING");
+});
+app.listen(port, () => {
+  console.log(`ELARA TRAVEL IS TRAVELLING ON PORT ${port}`);
+});
