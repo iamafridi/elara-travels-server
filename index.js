36bc5ec3773e8e2ddf99e352d0391bd7b0b43a2c
Afridi Akbar Ifty <afridiakbarifty@gmail.com>
2023-12-05 16:30:47 +0600

    Performed Delete Operation

--- index.js
+++ index.js
@@ -52,9 +52,16 @@ async function run() {
 
     // Reading here
     app.get("/carts", async (req, res) => {
-        const email = req.query.email;
-        const query= {email: email};
+      const email = req.query.email;
+      const query = { email: email };
       const result = await cartsCollection.find(query).toArray();
+      res.send(result);
+    });
+    // Deleting Here
+    app.delete("/carts/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await cartsCollection.deleteOne(query);
       res.send(result);
     });
 
