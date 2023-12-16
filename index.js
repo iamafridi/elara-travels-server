de7717cded9d39c4bd604ac03eca5c043a29d6bf
Afridi Akbar Ifty <afridiakbarifty@gmail.com>
2023-12-17 01:36:23 +0600

    Transiction through Stripe

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const jwt = require("jsonwebtoken");
 const cors = require("cors");
 const app = express();
 require("dotenv").config();
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const port = process.env.PORT || 5000;
 const ObjectId = require("mongodb").ObjectId;
 // MiddleWare
@@ -72,6 +73,22 @@ async function run() {
       }
       next();
     };
+
+    // Stripe Api
+    // PAYMENT INTENT
+    app.post("/create-payment-intent", async (req, res) => {
+      const { price } = req.body;
+      const amount = parseInt(price * 100);
+
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: 'usd',
+        payment_method_types: ['card'],
+      });
+      res.send({
+        clientSecret: paymentIntent.client_secret
+      })
+    });
 
     // User Related Api
     // Reading users
